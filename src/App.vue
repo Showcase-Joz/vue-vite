2eecb38566c25f21ae4b93eda755da672c8c33c6
Joz <jolyon@outfit.io>
2022-06-24 23:08:58 +0100

    streamline base setup

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,29 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import ShapesVue from './components/Shapes.vue'
-import LogosVue from './components/Logos.vue'
 </script>
 
 <template>
-    <div
-        id="app"
-        v-bind:style="{
-            backgroundImage: 'url(' + makeTemplateProps.backgroundImage + ')',
-        }"
-    >
-        <div v-if="makeTemplateProps.showLogos" style="z-index: 2">
-            <Logos />
-        </div>
-
-        <h1>{{ makeTemplateProps.headline }}</h1>
-
-        <Shapes />
+    <div>
+        <h1>{{ headline }}</h1>
     </div>
 </template>
 
 <script>
-import Logos from './components/Logos.vue'
-import Shapes from './components/Shapes.vue'
-
-const makeTemplateMixin = {
-    computed: {
-        makeTemplateProps() {
-            return (
-                window.templateProps || {
-                    headline: 'Hello World',
-                    showLogos: true,
-                    backgroundImage: '',
-                }
-            )
-        },
-    },
-}
-
 export default {
     name: 'App',
-    components: {
-        Logos,
-        Shapes,
+    data() {
+        return {
+            headline: 'Hello World',
+        }
     },
-    mixins: [makeTemplateMixin],
 }
 </script>
 
 <style>
 body {
     margin: 0;
-    font-family: 'IBM Plex Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-        'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
-        'Helvetica Neue', sans-serif;
+    font-family: monospace, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
 }
@@ -63,19 +31,11 @@ body {
 #app {
     width: 100vw;
     height: 100vh;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
     display: flex;
     justify-content: center;
     flex-direction: column;
     align-items: center;
     text-align: center;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
 }
 
 h1 {
@@ -83,9 +43,5 @@ h1 {
     font-weight: 700;
     line-height: 100%;
     color: #1f2a44;
-    text-align: center;
-    width: 100%;
-    padding: 0 2rem;
-    z-index: 1;
 }
 </style>
